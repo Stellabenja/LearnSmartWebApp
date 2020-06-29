ef8cce1874cc4f184d70f105bb0b6b6760e81781
Stellabenja <benjamine1994@gmail.com>
2020-06-29 14:27:55 +0200

    add forms

--- frontend/learnsmart-front/src/components/App.js
+++ frontend/learnsmart-front/src/components/App.js
@@ -4,12 +4,28 @@ import '../styles/App.css';
 import Header from './shared/Header';
 import Footer from './shared/Footer';
 import Home from './Home'
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./Login.js";
+import Signup from "./Signup.js"
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Home/>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/sign-in">
+            <Login/>
+            
+          </Route>
+          <Route path="/signup">
+          <Signup/>
+          </Route>
+        </Switch> 
+      </Router>
+     
       <Footer/>
     </div>
   );
--- frontend/learnsmart-front/src/components/shared/Header.js
+++ frontend/learnsmart-front/src/components/shared/Header.js
@@ -1,23 +1,29 @@
 import React , { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "../Login.js";
 import '../../styles/App.css';
 export default class Header extends Component{
     render(){
-        return ( <nav class="navbar navbar-expand-lg navbar-light bgnav-color ">
-            <a class="navbar-brand text-white"  href="#">Learnsmart</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                   
-                    <li class="nav-item">
-                        <a class="nav-link text-white t-18" href="#">Home</a>
-                    </li>
-                    <li class="nav-item float-right">
-                        <a class="nav-link text-white t-18"  href="#">Login</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        return ( 
+            <Router> 
+            <nav class="navbar navbar-expand-lg navbar-light bgnav-color ">
+                <a class="navbar-brand text-white"  href="#">Learnsmart</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                    
+                        <li class="nav-item">
+                        <Link className="nav-link text-white t-18" to="/">Home</Link>
+                            
+                        </li>
+                        <li class="nav-item float-right">
+                        <Link className="nav-link text-white t-18" to="/sign-in">Login</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+          </Router>
         )}
 }
