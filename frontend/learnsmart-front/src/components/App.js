5a52c9771b91078577b55ed06e7d171e9c904517
Stellabenja <benjamine1994@gmail.com>
2020-07-06 00:45:18 +0200

    comment code

--- frontend/learnsmart-front/src/components/App.js
+++ frontend/learnsmart-front/src/components/App.js
@@ -48,20 +48,36 @@ function App() {
     </div>
   );
 }
-/* componentDidMount() {
-  axios.get('http://localhost:5000/api/auth/logout',
-  {
-      headers: {
-      'Authorization':`Bearer ${localStorage.getItem('token')}`
-    }
-  }
-  )
-      .then(res => {
-          console.log(res.data)
-           this.setState({ redirect: true })
-      })
-      .catch(function (error) {
-          console.log(error);
-      })
-} */
 export default App;
+/*export default class Logout extends Component {
+constructor(props){
+    super(props)
+    this.state = {
+        redirect: false
+    }
+}
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/auth/logout',
+        {
+            headers: {
+            'Authorization':`Bearer ${localStorage.getItem('token')}`
+          }
+        }
+        )
+            .then(res => {
+                console.log(res.data)
+                 this.setState({ redirect: true })
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+    render() {
+        const { redirect } = this.state;
+        if (redirect) {
+        //return <Redirect to='/sign-in'/>;
+        }
+        return(<div>LOGOUT</div>) 
+    }
+} */
+
